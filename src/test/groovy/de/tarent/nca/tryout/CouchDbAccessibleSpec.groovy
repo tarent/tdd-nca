fefbf218745e8ada6411b22f21b2191de5c5c621
Tim Steffens <tsteffens@tarent.de>
2019-06-14 12:54:12 +0200

    rename CouchDbSpec to CouchDbAccessibleSpec

--- src/test/groovy/de/tarent/nca/tryout/CouchDbAccessibleSpec.groovy
+++ src/test/groovy/de/tarent/nca/tryout/CouchDbAccessibleSpec.groovy
@@ -2,16 +2,19 @@ package de.tarent.nca.tryout
 
 import de.tarent.nca.testutil.CouchDb
 import de.tarent.nca.testutil.NeedsCouchDb
+import de.tarent.nca.testutil.RestClientFactory
 import groovyx.net.http.RESTClient
 import spock.lang.Specification
 
+import static de.tarent.nca.testutil.RestClientFactory.createRESTClient
+
 @NeedsCouchDb
-class CouchDbSpec extends Specification {
+class CouchDbAccessibleSpec extends Specification {
 
     def restClient
 
     def setup() {
-        restClient = new RESTClient(CouchDb.instance.getUrl())
+        restClient = createRESTClient(CouchDb.instance.getUrl())
     }
 
     def cleanup() {
